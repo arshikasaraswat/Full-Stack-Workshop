ec6d5e3293d1952f2dc180615982d1a86820811b
Arshika <saraswatarshika@gmail.com>
2023-11-25 11:51:42 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,11 @@
+function equation(val){
+document.getElementById("txt1").value+=val;
+}
+function calculation(val){
+    vara=document.getElementById("txt1").value
+var c=eval(a)
+document.getElementById("txt1").value=c;
+}
+function clearC(val){
+document.getElementById("txt1").value="";
+}
